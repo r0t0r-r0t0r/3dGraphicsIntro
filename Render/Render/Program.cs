2900c37a29e4157b16fdb57814a2432ff935429e
r0t0r-r0t0r <ultrauser777@gmail.com>
2015-01-29 23:08:32 +0300

    Created Visual Studio solution based on linq-file

---
+++ Render/Render/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Render
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RenderForm());
+        }
+    }
+}
