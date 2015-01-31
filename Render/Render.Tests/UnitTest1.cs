504e5ec1c6b530f1b3d31a6ece55952d19936726
r0t0r-r0t0r <ultrauser777@gmail.com>
2015-01-31 23:10:31 +0300

    Simple lighting

---
+++ Render/Render.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Drawing;
+
+namespace Render.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void Test()
+        {
+            var bitmap = new Bitmap(50, 50);
+            RenderCore.Triangle(10, 20, 40, 20, 60, 60, bitmap, Color.Red);
+        }
+    }
+}
