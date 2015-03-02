7f680acfbffcd93f945cc40da5a9b03375522cd4
r0t0r-r0t0r <ultrauser777@gmail.com>
2015-03-02 22:57:46 +0300

    First part of matrix transformations

--- Render/Render/RenderCore.cs
+++ Render/Render/RenderCore.cs
@@ -133,6 +133,7 @@ namespace Render
         private unsafe static void Draw(Model model, byte* data, int width, int height, List<IRender> renders, float cameraZPosition, bool usePerspectiveProjection, Vector3 cameraDirection, IPixelShader shader, int startY, int endY)
         {
             float c = cameraZPosition;
+            var transform = Mul(Viewport(0, 0, width, height), Projection(cameraZPosition));
 
             foreach (var face in model.Faces)
             {
@@ -141,23 +142,26 @@ namespace Render
                 for (var j = 0; j < 3; j++)
                 {
                     var modelCoord = model.Vertices[face[j]];
-                    var lookAt = LookAt(new Vector3(0, 0, 0), new Vector3(5, 0, 10), new Vector3(0, 1, 0));
-                    var foo = Mul(lookAt, modelCoord);
-                    modelCoord = foo;
+                    //var view = LookAt(new Vector3(0, 0, 0), new Vector3(0, 0, 10), new Vector3(1, 1, 0));
+                    //var foo = Mul(view, modelCoord);
+                    ////modelCoord = foo;
                         
-                    var w = 1 - modelCoord.Z/c;
-                    var g = new Vector3(modelCoord.X*300, modelCoord.Y*300, modelCoord.Z*300);
-                    if (usePerspectiveProjection)
-                    {
-                        screenCoords[j] = new Vector3(g.X/w, g.Y/w, g.Z/w);
-                    }
-                    else
-                    {
-                        screenCoords[j] = new Vector3(g.X, g.Y, g.Z);
-                    }
+                    //var w = 1 - modelCoord.Z/c;
+                    //var g = new Vector3(modelCoord.X*300, modelCoord.Y*300, modelCoord.Z*300);
+                    //if (usePerspectiveProjection)
+                    //{
+                    //    screenCoords[j] = new Vector3(g.X/w, g.Y/w, g.Z/w);
+                    //}
+                    //else
+                    //{
+                    //    screenCoords[j] = new Vector3(g.X, g.Y, g.Z);
+                    //}
 
-                    screenCoords[j] = new Vector3(screenCoords[j].X/(cameraZPosition*0.1f) + width/2,
-                        screenCoords[j].Y/(cameraZPosition*0.1f) + height/2, screenCoords[j].Z);
+                    //screenCoords[j] = new Vector3(screenCoords[j].X/(cameraZPosition*0.1f) + width/2,
+                    //    screenCoords[j].Y/(cameraZPosition*0.1f) + height/2, screenCoords[j].Z);
+
+                    var r = Mul(transform, new Vector4(modelCoord, 1));
+                    screenCoords[j] = new Vector3(r.X/r.W, r.Y/r.W, r.Z/r.W);
                 }
 
                 var v0 = screenCoords[0];
@@ -199,6 +203,40 @@ namespace Render
             return result;
         }
 
+        private static Matrix4x4 Viewport(int x, int y, int width, int height)
+        {
+            const int depth = 255;
+
+            var hw = (float)width/2;
+            var hh = (float)height/2;
+            var hd = (float)depth/2;
+
+            var result = new Matrix4x4(
+                hw, 0, 0, x + hw,
+                0, hh, 0, y + hh,
+                0, 0, hd, hd,
+                0, 0, 0, 1
+                );
+            return result;
+        }
+
+        private static Matrix4x4 Model()
+        {
+            var result = Matrix4x4.Identity;
+            return result;
+        }
+
+        private static Matrix4x4 Projection(float d)
+        {
+            var result = new Matrix4x4(
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, -1/d, 1
+                );
+            return result;
+        }
+
         private static Vector3 Mul(Matrix4x4 m, Vector3 vector)
         {
             var v = new Vector4(vector, 1);
@@ -206,6 +244,48 @@ namespace Render
                 m.M11*v.X + m.M12*v.Y + m.M13*v.Z + m.M14*v.W,
                 m.M21*v.X + m.M22*v.Y + m.M23*v.Z + m.M24*v.W,
                 m.M31*v.X + m.M32*v.Y + m.M33*v.Z + m.M34*v.W
+                );
+            return result;
+        }
+
+        private static Vector4 Mul(Matrix4x4 m, Vector4 v)
+        {
+            var result = new Vector4(
+                m.M11 * v.X + m.M12 * v.Y + m.M13 * v.Z + m.M14 * v.W,
+                m.M21 * v.X + m.M22 * v.Y + m.M23 * v.Z + m.M24 * v.W,
+                m.M31 * v.X + m.M32 * v.Y + m.M33 * v.Z + m.M34 * v.W,
+                m.M41 * v.X + m.M42 * v.Y + m.M43 * v.Z + m.M44 * v.W
+                );
+            return result;
+        }
+
+        private static Matrix4x4 Mul(Matrix4x4 m1, Matrix4x4 m2)
+        {
+            var m11 = m1.M11*m2.M11 + m1.M12*m2.M21 + m1.M13*m2.M31 + m1.M14*m2.M41;
+            var m12 = m1.M11*m2.M12 + m1.M12*m2.M22 + m1.M13*m2.M32 + m1.M14*m2.M42;
+            var m13 = m1.M11*m2.M13 + m1.M12*m2.M23 + m1.M13*m2.M33 + m1.M14*m2.M43;
+            var m14 = m1.M11*m2.M14 + m1.M12*m2.M24 + m1.M13*m2.M34 + m1.M14*m2.M44;
+
+            var m21 = m1.M21*m2.M11 + m1.M22*m2.M21 + m1.M23*m2.M31 + m1.M24*m2.M41;
+            var m22 = m1.M21*m2.M12 + m1.M22*m2.M22 + m1.M23*m2.M32 + m1.M24*m2.M42;
+            var m23 = m1.M21*m2.M13 + m1.M22*m2.M23 + m1.M23*m2.M33 + m1.M24*m2.M43;
+            var m24 = m1.M21*m2.M14 + m1.M22*m2.M24 + m1.M23*m2.M34 + m1.M24*m2.M44;
+
+            var m31 = m1.M31*m2.M11 + m1.M32*m2.M21 + m1.M33*m2.M31 + m1.M34*m2.M41;
+            var m32 = m1.M31*m2.M12 + m1.M32*m2.M22 + m1.M33*m2.M32 + m1.M34*m2.M42;
+            var m33 = m1.M31*m2.M13 + m1.M32*m2.M23 + m1.M33*m2.M33 + m1.M34*m2.M43;
+            var m34 = m1.M31*m2.M14 + m1.M32*m2.M24 + m1.M33*m2.M34 + m1.M34*m2.M44;
+
+            var m41 = m1.M41*m2.M11 + m1.M42*m2.M21 + m1.M43*m2.M31 + m1.M44*m2.M41;
+            var m42 = m1.M41*m2.M12 + m1.M42*m2.M22 + m1.M43*m2.M32 + m1.M44*m2.M42;
+            var m43 = m1.M41*m2.M13 + m1.M42*m2.M23 + m1.M43*m2.M33 + m1.M44*m2.M43;
+            var m44 = m1.M41*m2.M14 + m1.M42*m2.M24 + m1.M43*m2.M34 + m1.M44*m2.M44;
+
+            var result = new Matrix4x4(
+                m11, m12, m13, m14,
+                m21, m22, m23, m24,
+                m31, m32, m33, m34,
+                m41, m42, m43, m44
                 );
             return result;
         }
