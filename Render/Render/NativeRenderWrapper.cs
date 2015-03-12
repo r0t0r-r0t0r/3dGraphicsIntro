35bde17a50b1c98bff03c5613db7deb3d63d08a2
r0t0r-r0t0r <ultrauser777@gmail.com>
2015-03-12 22:54:54 +0300

    Created project for native code

---
+++ Render/Render/NativeRenderWrapper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Render
+{
+    public static class NativeRenderWrapper
+    {
+        [DllImport("NativeRender.dll", EntryPoint = "?fnNativeRender@@YAHXZ")]
+        public static extern int fnNativeRender();
+
+        [DllImport("NativeRender.dll", EntryPoint = "?GetIntensity@@YAMHMMM@Z", CallingConvention = CallingConvention.Cdecl)]
+        public static extern float GetIntensity(int packedNormal, float lightX, float lightY, float lightZ);
+    }
+}
